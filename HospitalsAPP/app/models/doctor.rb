47e21bc003ff9ad6dbc6f8eafce21244e95a5560
CS31IvanK <kaftanatii2021ks12@student.karazin.ua>
2024-03-24 20:42:52 +0100

    Changing htmls and logic

--- HospitalsAPP/app/models/doctor.rb
+++ HospitalsAPP/app/models/doctor.rb
@@ -1,6 +1,6 @@
 class Doctor < ApplicationRecord
   belongs_to :hospital, foreign_key: "hos_id", primary_key: "rank" #???
   #validation
-  validates :email, format: { with: /\A[\w+\-.]+@karazin\.ua\z/,
-                              message: "must be a karazin.ua account" }
+  validates :dphone, format: { with: /\A?0\d{9}\z/,
+                               message: "must be a valid phone number" }
 end
