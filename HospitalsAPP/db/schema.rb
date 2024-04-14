fe7872af4a89caaa8a210f2dd99499a2fe2ca53a
CS31IvanK <kaftanatii2021ks12@student.karazin.ua>
2024-04-14 18:35:45 +0200

    Add sockets changed files

--- HospitalsAPP/db/schema.rb
+++ HospitalsAPP/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,94 +10,120 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_325_093_030) do # rubocop:todo Metrics/BlockLength
-  create_table 'doctors', force: :cascade do |t|
-    t.integer 'dock_id'
-    t.string 'dname'
-    t.string 'spec'
-    t.string 'dphone'
-    t.integer 'hos_id'
-    t.integer 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema[7.1].define(version: 2024_04_14_163127) do
+  create_table "cards", force: :cascade do |t|
+    t.integer "card_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+    create_table 'doctors', force: :cascade do |t|
+      t.integer 'dock_id'
+      t.string 'dname'
+      t.string 'spec'
+      t.string 'dphone'
+      t.integer 'hos_id'
+      t.integer 'user_id'
+      t.datetime 'created_at', null: false
+      t.datetime 'updated_at', null: false
+      end
+  create_table "conversations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "doctor_id", null: false
+    t.integer "patient_id", null: false
+    t.index ["doctor_id"], name: "index_conversations_on_doctor_id"
+    t.index ["patient_id"], name: "index_conversations_on_patient_id"
+  end
+  create_table "hospitals", force: :cascade do |t|
+    t.integer "rank"
+    t.string "name"
+    t.string "url"
+    t.string "country"
+    t.string "city"
   end
 
-  create_table 'hospitals', force: :cascade do |t|
-    t.integer 'rank'
-    t.string 'name'
-    t.string 'url'
-    t.string 'country'
-    t.string 'city'
+  create_table "medical_cards", force: :cascade do |t|
+    t.integer "medical_card_id"
+    t.integer "patient_id"
+    t.integer "doctor_id"
+    t.integer "hospital_id"
+    t.string "diagnosis"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "appointment_date"
+    t.boolean "attended", default: false
+    t.string "appointment_type"
+    t.index ["doctor_id"], name: "index_medical_cards_on_doctor_id"
+    t.index ["hospital_id"], name: "index_medical_cards_on_hospital_id"
+    t.index ["patient_id"], name: "index_medical_cards_on_patient_id"
   end
 
-  create_table 'medical_cards', force: :cascade do |t|
-    t.integer 'medical_card_id'
-    t.integer 'patient_id'
-    # t.integer "doctor_id"
-    t.integer 'hospital_id'
-    # t.string "diagnosis"
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.datetime 'appointment_date'
-    t.boolean 'attended', default: false
-    t.string 'appointment_type'
-    t.index ['doctor_id'], name: 'index_medical_cards_on_doctor_id'
-    t.index ['hospital_id'], name: 'index_medical_cards_on_hospital_id'
-    t.index ['patient_id'], name: 'index_medical_cards_on_patient_id'
+  create_table "medical_records", force: :cascade do |t|
+    t.integer "mrec_id"
+    t.datetime "date"
+    t.string "problem"
+    t.integer "pat_id"
+    t.integer "doc_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "medical_card_id"
+    t.index ["medical_card_id"], name: "index_medical_records_on_medical_card_id"
   end
 
-  create_table 'medical_records', force: :cascade do |t|
-    t.integer 'mrec_id'
-    t.datetime 'date'
-    t.string 'problem'
-    t.integer 'pat_id'
-    t.integer 'doc_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'medical_card_id'
-    t.index ['medical_card_id'], name: 'index_medical_records_on_medical_card_id'
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "body"
+    t.integer "conversation_id", null: false
+    t.integer "user_id", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table 'patients', force: :cascade do |t|
-    t.integer 'pat_id'
-    t.string 'pname'
-    t.string 'padress'
-    t.string 'pphone'
-    t.integer 'age'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
-    t.index ['user_id'], name: 'index_patients_on_user_id'
+  create_table "patients", force: :cascade do |t|
+    t.integer "pat_id"
+    t.string "pname"
+    t.string "padress"
+    t.string "pphone"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_patients_on_user_id"
   end
 
-  create_table 'records', force: :cascade do |t|
-    t.string 'record_id'
-    t.string 'conclusion'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'dock_id'
-    t.integer 'pat_id'
-    t.index ['dock_id'], name: 'index_records_on_dock_id'
-    t.index ['pat_id'], name: 'index_records_on_pat_id'
+  create_table "records", force: :cascade do |t|
+    t.string "record_id"
+    t.string "conclusion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "dock_id"
+    t.integer "pat_id"
+    t.index ["dock_id"], name: "index_records_on_dock_id"
+    t.index ["pat_id"], name: "index_records_on_pat_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'role'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'doctors', 'users'
-  add_foreign_key 'medical_cards', 'doctors'
-  add_foreign_key 'medical_cards', 'hospitals'
-  add_foreign_key 'medical_cards', 'patients'
-  add_foreign_key 'medical_records', 'medical_cards'
-  add_foreign_key 'patients', 'users'
+  add_foreign_key "conversations", "users", column: "doctor_id"
+  add_foreign_key "conversations", "users", column: "patient_id"
+  add_foreign_key "doctors", "users"
+  add_foreign_key "medical_cards", "doctors"
+  add_foreign_key "medical_cards", "hospitals"
+  add_foreign_key "medical_cards", "patients"
+  add_foreign_key "medical_records", "medical_cards"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
+  add_foreign_key "patients", "users"
 end
